d9ec301b8d0d842aad4947d4e798507e18a7201f
wuzhongwulidong <707881742@qq.com>
2020-08-08 17:07:38 +0800

    commit_1

---
+++ cmake-build-debug/CMakeFiles/myFirstClionProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myFirstClionProject.dir/main.cpp.o"
+  "myFirstClionProject"
+  "myFirstClionProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myFirstClionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
